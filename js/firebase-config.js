7851b1ebc926f618a2ac7a21bbbd4cd1cd21cc23
pakik <paki.kova@gmail.com>
2026-03-22 20:07:51 +0100

    Initialize Firebase project and add essential files for the election dashboard. Includes configuration files (.firebaserc, firebase.json), a favicon (favicon.svg), main HTML structure (index.html), and data files (locations.json, bor.json, bor.geojson). Also includes Firebase configuration script (firebase-config.js).

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBB2yq4b2lLGhI8PMD7NuXkUa6qRHO1FoM",
+    authDomain: "izbori-bor-2026.firebaseapp.com",
+    projectId: "izbori-bor-2026",
+    storageBucket: "izbori-bor-2026.firebasestorage.app",
+    messagingSenderId: "834953615970",
+    appId: "1:834953615970:web:5d57d2ad054bdc2e230f3b"
+};
+
+firebase.initializeApp(firebaseConfig);
